ad9bce923e30e271cb725919d055ab7569fcd84c
mlinglhl <minhung.ling@gmail.com>
2017-01-13 19:15:49 -0500

    Initial Commit

---
+++ 20170113 Snakes and Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  20170113 Snakes and Ladders
+//
+//  Created by Minhung Ling on 2017-01-13.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
